2475e5048e7fb0a01f579351d147570e030421c7
Alessandro Biavati <abiavati@gmail.com>
2019-07-01 00:27:08 -0700

    add tmux alias and git alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -23,11 +23,14 @@ alias dotfiles="cd $DOTFILES_DIR"
 alias reload="source ~/.config/fish/config.fish"
 
 # Dorcker
-alias docker-stop-all-containers='docker stop $(docker ps -a -q)'
-alias docker-rm-all-containers='docker rm $(docker ps -a -q)'
-alias docker-rm-all-images='docker rmi $(docker images -a -q)'
-alias docker-rm-all-volumes='docker volume rm $(docker volume ls -f dangling=true -q)'
-alias docker-reset='(docker-stop-all-containers > /dev/null 2>&1) && (docker-rm-all-containers > /dev/null 2>&1) && (docker-rm-all-images > /dev/null 2>&1) && (docker-rm-all-volumes > /dev/null 2>&1)'
+alias docker-stop-all-containers="docker stop (docker ps -a -q)"
+alias docker-rm-all-containers="docker rm (docker ps -a -q)"
+alias docker-rm-all-images="docker rmi (docker images -a -q)"
+alias docker-rm-all-volumes="docker volume rm (docker volume ls -f dangling=true -q)"
+# alias docker-reset="(docker-stop-all-containers > /dev/null 2>&1) && (docker-rm-all-containers > /dev/null 2>&1) && (docker-rm-all-images > /dev/null 2>&1) && (docker-rm-all-volumes > /dev/null 2>&1)"
 
 # Verdaccio
 alias verdaccio="docker run -d -it --rm --name verdaccio -p 4873:4873 verdaccio/verdaccio"
+
+# Tmux
+alias t="tmux attach || tmux new -s main"
