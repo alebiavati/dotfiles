2d993cfa3623a96acaefd1c77acb581da63091ba
Alessandro Biavati <abiavati@gmail.com>
2020-03-12 00:04:55 -0700

    fix traefik

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,4 +1,4 @@
 set -x CODE_DIR "$HOME/code"
 set -x DOTFILES_DIR "$CODE_DIR/.dotfiles"
 set -x TRAEFIK_DIR "$CODE_DIR/.traefik"
-set -x PATH "$HOME/bin:$HOME/.local/bin:$HOME/.composer/vendor/bin:$TRAEFIK_DIR/bin:$PATH"
+set -x PATH "$TRAEFIK_DIR/bin:$HOME/bin:$HOME/.local/bin:$HOME/.composer/vendor/bin:$PATH"
--- scripts/traefik.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# install traefik repository
-setup_traefik () {
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/zerooneagency/traefik/master/bin/traefik)"
-}
