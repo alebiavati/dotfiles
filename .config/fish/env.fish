7927ad425c540d143b61d74d495f20a3a5865ff6
Alessandro Biavati <abiavati@gmail.com>
2020-03-11 23:59:57 -0700

    add traefik

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,3 +1,4 @@
-set -x PATH "$HOME/bin:$HOME/.local/bin:$HOME/.composer/vendor/bin:$PATH"
 set -x CODE_DIR "$HOME/code"
 set -x DOTFILES_DIR "$CODE_DIR/.dotfiles"
+set -x TRAEFIK_DIR "$CODE_DIR/.traefik"
+set -x PATH "$HOME/bin:$HOME/.local/bin:$HOME/.composer/vendor/bin:$TRAEFIK_DIR/bin:$PATH"
