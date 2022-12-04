e55588aad282027752f525cd12fa2484803b64a2
Alessandro Biavati <alessandro@allyassist.com.au>
2022-12-04 17:15:38 +1100

    update brew

--- .config/fish/gcloud.fish
+++ .config/fish/gcloud.fish
@@ -1,15 +1,32 @@
 # python for gcloud
-set -x CLOUDSDK_PYTHON /usr/local/bin/python3
+if test -e /opt/homebrew/bin/python3
+  set -x CLOUDSDK_PYTHON /opt/homebrew/bin/python3
+end
+
+if test -e /usr/local/bin/python3
+  set -x CLOUDSDK_PYTHON /usr/local/bin/python3
+end
 
 # The next line updates PATH for the Google Cloud SDK.
 if status --is-interactive
-  set gcloud_inc_file /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+  set gcloud_inc_file_local /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+  if [ -f $gcloud_inc_file_local ]
+    source $gcloud_inc_file_local
+  end
+
+  # The next line enables shell command completion for gcloud.
+  set gcloud_completion_inc_file_local /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
+  if [ -f $gcloud_completion_inc_file_local ]
+    source $gcloud_completion_inc_file_local
+  end
+
+  set gcloud_inc_file /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
   if [ -f $gcloud_inc_file ]
     source $gcloud_inc_file
   end
 
   # The next line enables shell command completion for gcloud.
-  set gcloud_completion_inc_file /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
+  set gcloud_completion_inc_file /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc
   if [ -f $gcloud_completion_inc_file ]
     source $gcloud_completion_inc_file
   end
--- .config/fish/make.fish
+++ .config/fish/make.fish
@@ -2,3 +2,7 @@
 if test -e /usr/local/opt/make/libexec/gnubin
   set -g fish_user_paths "/usr/local/opt/make/libexec/gnubin" $fish_user_paths
 end
+
+if test -e /opt/homebrew/opt/make/libexec/gnubin
+  set -g fish_user_paths "/opt/homebrew/opt/make/libexec/gnubin" $fish_user_paths
+end
